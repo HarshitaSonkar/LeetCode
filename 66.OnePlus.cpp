4e1ea953bc8970ac3e6b9c9c6358fb54a5b9d8fb
HarshitaSonkar <58362202+HarshitaSonkar@users.noreply.github.com>
2019-12-17 19:22:53 +0530

    Create 66.OnePlus.cpp

---
+++ 66.OnePlus.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+        int carry;
+        for(int i=digits.size()-1;i>=0;i--){
+            
+            if(digits[i]>=9){
+                digits[i]=0;
+               carry=1;
+            }else{
+                digits[i]+=1;
+                carry=0;
+                break;
+            }
+         
+       }
+        if(carry){
+            
+            digits.insert(digits.begin(),1);
+        }
+         return digits;
+    }
+};
