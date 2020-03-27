8bad6901569d685569e8d746ba0b233a78648485
HarshitaSonkar <58362202+HarshitaSonkar@users.noreply.github.com>
2020-03-27 12:24:55 +0530

    Create 977.SquaresofaSortedArray.cpp

---
+++ ARRAY/977.SquaresofaSortedArray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        
+        for(int & i : A){
+            i = i*i;
+        }
+        
+        sort(A.begin(), A.end());
+        
+        return A;
+    }
+};
+
+ 
