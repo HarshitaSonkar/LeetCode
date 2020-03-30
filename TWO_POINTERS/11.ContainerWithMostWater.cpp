f4dc666541d76cd4d0f1cc557e88f8f3097e406d
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-03-30 17:19:41 +0530

    Create 11.ContainerWithMostWater.cpp

---
+++ TWO_POINTERS/11.ContainerWithMostWater.cpp
@@ -0,0 +1,85 @@
+
+        
+     class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        int i=0;
+        int j=height.size()-1;
+        int cap1=0;
+        int cap2=0;
+        int m=0;
+        int max1=0;
+        int max2=0;
+        
+        while(i<j){
+            if(height[i]<height[j]){
+                cap1=height[i]*(j-i);
+                    if(cap1>max1){
+                        max1=cap1;
+                    }
+                i++;
+              }else{
+                  cap2=height[j]*(j-i);
+                if(cap2>max2){
+                    max2=cap2;
+                }
+                  j--;
+             }
+            m=max(max1,max2);
+        }
+        return m;
+    }
+};
+        
+        
+        
+        
+        
+        
+        
+        /*
+        int maxArea(vector<int>& height) {
+    int water = 0;
+    int i = 0, j = height.size() - 1;
+    while (i < j) {
+        int h = min(height[i], height[j]);
+        water = max(water, (j - i) * h);
+        while (height[i] <= h && i < j) i++;
+        while (height[j] <= h && i < j) j--;
+    }
+    return water;
+}
+        
+        
+        
+        
+        
+      int i=0;
+        int j= height.size()-1;
+        int cap;
+        int max=0;
+        while(i<j) { 
+            if(height[i]<height[j]){ 
+            cap=height[i]*(j-i);
+             
+                // if(cap>max)
+                 //max=cap;
+                i++;
+                       
+            }else{
+                cap=height[j]*(j-i);
+               
+                 //if(cap>max)
+                 //max=cap;
+                 j--;
+                        
+            } 
+                if(cap>max)
+                 max=cap;
+                              
+            
+        }
+        return cap;
+    }
+};   */
