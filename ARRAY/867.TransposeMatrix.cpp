999b46da565de4591275db44c2870ef2e5ad7dc8
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-04-04 19:06:50 +0530

    Create 867.TransposeMatrix.cpp

---
+++ ARRAY/867.TransposeMatrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) {
+         int col = A[0].size();
+         int row = A.size();
+        vector<vector<int>> res(col,vector<int>(row,0));
+        
+        for(int i = 0; i < row;i++){
+            for(int j = 0;j < col;j++){
+                res[j][i] = A[i][j];
+            }
+        }
+        return res;
+    }
+};
