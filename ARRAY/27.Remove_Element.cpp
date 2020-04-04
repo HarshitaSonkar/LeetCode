acfe45bf2a34e93eb772f6e96af08c4324cd65ca
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-04-04 19:08:00 +0530

    Delete 27.Remove_Element.cpp

--- ARRAY/27.Remove_Element.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-       
-     return  std::distance(nums.begin(), std::remove(nums.begin(),nums.end(),val));
-
-    }
-};
