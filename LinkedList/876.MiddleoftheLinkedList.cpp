df6b71ffc1f1dd0b25f56fd960b84b8eb1e219a8
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-04-06 00:16:24 +0530

    Create 876.MiddleoftheLinkedList.cpp

---
+++ LinkedList/876.MiddleoftheLinkedList.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *first=head;
+        ListNode *second=head;
+         while(second && second->next){
+             first=first->next;
+             second=second->next->next;
+         }
+        return first;
+    }
+};
