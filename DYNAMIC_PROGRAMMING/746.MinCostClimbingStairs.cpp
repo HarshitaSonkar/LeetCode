a1d5711c6f53a47c0ca835205bda1b64f8a274bc
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-05-21 23:01:03 +0530

    Update 746.MinCostClimbingStairs.cpp

--- DYNAMIC_PROGRAMMING/746.MinCostClimbingStairs.cpp
+++ DYNAMIC_PROGRAMMING/746.MinCostClimbingStairs.cpp
@@ -12,5 +12,27 @@ public:
             x = x1;
         }
         return min(x,y);
+        /*
+        ******************************************************************************
+          for (int i = 2; i < cost.size(); i++) {
+            cost[i] += min(cost[i - 1], cost[i - 2]);            
+        }
+        return min(cost[cost.size() - 1], cost[cost.size() - 2]);
+        */
+        /*
+        *******************************************************************************
+        int n=cost.size();
+        int dp[n+1];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        cost.push_back(0);
+        
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=min(dp[i-1]+cost[i],dp[i-2]+cost[i]);
+            
+        }
+        return dp[n];
+        */
     }
 };
