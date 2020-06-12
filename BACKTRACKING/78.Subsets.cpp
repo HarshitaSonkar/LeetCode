91efb034c8f86311f869ef0814a47f53d4b85e01
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-06-12 13:05:13 +0530

    Update 78.Subsets.cpp

--- BACKTRACKING/78.Subsets.cpp
+++ BACKTRACKING/78.Subsets.cpp
@@ -12,11 +12,12 @@ private:
     void subsets(vector<int>&nums, vector<vector<int>>&res, vector<int>&ans, int i){
          if(i == nums.size()){
              res.push_back(ans);
-             return;
+             return;                                         // once we reach the end, add it to the result
          }
-        ans.push_back(nums[i]);
+        // for every element we have two choices : keep it or drop it
+        ans.push_back(nums[i]);                             //   choice 1, keep it
         subsets(nums, res, ans, i+1);
-        ans.pop_back();
+        ans.pop_back();                                    // choice 2, drop it
         subsets(nums, res, ans, i+1);
         
      }
