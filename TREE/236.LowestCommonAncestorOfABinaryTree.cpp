1ec4069283c063fc213ad140cb118722f6c086ea
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-07-05 00:00:05 +0530

    Update 236.LowestCommonAncestorOfABinaryTree.cpp

--- TREE/236.LowestCommonAncestorOfABinaryTree.cpp
+++ TREE/236.LowestCommonAncestorOfABinaryTree.cpp
@@ -11,11 +11,22 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-    if (!root || root == p || root == q) return root;
+        if(root == nullptr) return root;
+        if(root == p || root == q) return root;
+        
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        
+        if(left != nullptr && right != nullptr) return root;
+        if(left == nullptr && right == nullptr) return nullptr;
+        return (left != nullptr ? left : right);
+    }
+};
+
+/*
+ if (!root || root == p || root == q) return root;
     
     TreeNode* left = lowestCommonAncestor(root->left, p, q);
     TreeNode* right = lowestCommonAncestor(root->right, p, q);
     return !left ? right : !right ? left : root;
-    
-    }
-};
+    */
