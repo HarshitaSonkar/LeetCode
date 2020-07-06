f0bd9b21c49ab47783899ff00efce340e654e297
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-07-06 22:50:47 +0530

    Update 73.SetMatrixZeroes.cpp

--- ARRAY/73.SetMatrixZeroes.cpp
+++ ARRAY/73.SetMatrixZeroes.cpp
@@ -2,6 +2,40 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         
+ // solution 1
+        long long int r=A.size(),c=A[0].size(),i,j;
+   vector row(r,false); 
+   vector col(c,false); 
+   for(i=0;i<r;i++)
+     {
+       for(j=0;j<c;j++)
+          {
+            if(A[i][j]==0)
+              {
+                row[i]=true; 
+                col[j]=true; 
+               }
+           }
+     }
+    for(i=0;i<row.size();i++) // setting all the elements in respective rows with zero…
+       {
+        if(row[i])
+        for(j=0;j<c;j++)
+        A[i][j]=0;
+    }
+    for(i=0;i<col.size();i++) // setting all the elements in respective columns with zero…
+     {
+         if(col[i])
+         for(j=0;j<r;j++)
+         A[j][i]=0;
+      }
+  }
+};
+        
+        
+
+        
+ // solution 2
         bool row = false, col = false;
         for(int i = 0; i < matrix.size(); i++){
             for(int j = 0; j < matrix[0].size(); j++){
@@ -32,9 +66,9 @@ public:
 
 
 
-
+// solution 3
         
-   /*     int tmp = -1, n = matrix.size(), m = matrix[0].size(); 
+        int tmp = -1, n = matrix.size(), m = matrix[0].size(); 
         for(int i = 0 ; i < n; i++)
         {
             if(matrix[i][0] == 0)//when first column of that row is zero
@@ -51,6 +85,6 @@ public:
                     matrix[i][j] = 0;
             if(tmp == 0)//first coumn zero
                 matrix[i][0] = 0;                       
-        }  */
+        }  
         
   
