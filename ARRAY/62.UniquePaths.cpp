27da62880307975ae791439efb7ab5cae9976143
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-07-29 13:05:13 +0530

    Update 62.UniquePaths.cpp

--- ARRAY/62.UniquePaths.cpp
+++ ARRAY/62.UniquePaths.cpp
@@ -8,6 +8,15 @@ public:
                 res[j] += res[j - 1];
             }
         }
-        return res[n - 1];
+        return res[n - 1];                    // space: o(n)
     }
 };
+*******************Solution 2***************time & space : O(m*n)*****************************
+
+vector<vector<int>> dp(m, vector<int>(n, 1));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
